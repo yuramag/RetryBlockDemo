4ec85894ef6db12ff752f50996e46087f03277e3
Yuriy Magurdumov <yuriy_magurdumov@mednax.com>
2017-05-03 16:21:56 -0400

    Moving files in

---
+++ RetryBlockDemo/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RetryBlockDemo
+{
+    class Program
+    {
+        private enum MessageLevel
+        {
+            None,
+            Info,
+            Warning,
+            Error
+        }
+
+        private static void Main(string[] args)
+        {
+            var e = new ManualResetEventSlim();
+            RunDemosAsync().ContinueWith(x => e.Set());
+            e.Wait();
+
+            Log();
+            Log("Press <Enter> to exit...");
+            Console.ReadLine();
+        }
+
+        public static async Task Demo1Async()
+        {
+            var ct = CancellationToken.None;
+            await RetryBlock.ExecuteAsync<InvalidOperationException>(DemoActionAsync, 3, TimeSpan.FromSeconds(1), ErrorHandler, ct);
+        }
+
+        static async Task RunDemosAsync()
+        {
+            await RunDemoAsync(Demo1Async);
+        }
+
+        public static Task RunDemoAsync(Func<Task> demo)
+        {
+            var task = demo();
+            task.ContinueWith(x => Log("Result: Success", MessageLevel.Info), TaskContinuationOptions.OnlyOnRanToCompletion);
+            task.ContinueWith(x => Log("Result: Error => " + x.Exception.InnerException.Message, MessageLevel.Error), TaskContinuationOptions.OnlyOnFaulted);
+            return task;
+        }
+
+        static async Task DemoActionAsync()
+        {
+            await Task.Delay(10); // simulate some work
+            if (new Random().Next(5) != 0)
+                throw new InvalidOperationException("Exception has been thrown");
+            //Log("Success", MessageLevel.Info);
+        }
+
+        static void ErrorHandler<T>(T exception) where T : Exception
+        {
+            Log(exception.Message, MessageLevel.Error);
+        }
+
+        private static readonly object s_lock = new object();
+
+        static void Log(string message = null, MessageLevel level = MessageLevel.None)
+        {
+            lock (s_lock)
+            {
+                var oldColor = Console.ForegroundColor;
+                Console.ForegroundColor = level == MessageLevel.Info ? ConsoleColor.Green : level == MessageLevel.Warning ? ConsoleColor.Yellow : level == MessageLevel.Error ? ConsoleColor.Red : Console.ForegroundColor;
+                Console.WriteLine(message);
+                Console.ForegroundColor = oldColor;
+            }
+        }
+    }
+}
---
+++ RetryBlockDemo/RetryBlock.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RetryBlockDemo
+{
+    public static class RetryBlock
+    {
+        public static async Task ExecuteAsync<T>(Func<Task> action, int count, TimeSpan delay, Action<T> handler, CancellationToken ct) where T : Exception
+        {
+            var retry = 0;
+            while (true)
+            {
+                try
+                {
+                    if (retry > 0)
+                        await Task.Delay(delay, ct);
+                    await action();
+                    break;
+                }
+                catch (T ex)
+                {
+                    retry++;
+                    if (ex is OperationCanceledException)
+                        throw;
+                    if (handler != null)
+                        handler(ex);
+                    if (count >= 0 && retry >= count)
+                        throw;
+                }
+            }
+        }
+
+        public static async Task<TResult> ExecuteAsync<T, TResult>(Func<Task<TResult>> func, int count, TimeSpan delay, Action<T> handler, CancellationToken ct) where T : Exception
+        {
+            var retry = 0;
+            while (true)
+            {
+                try
+                {
+                    if (retry > 0)
+                        await Task.Delay(delay, ct);
+                    return await func();
+                }
+                catch (T ex)
+                {
+                    retry++;
+                    if (ex is OperationCanceledException)
+                        throw;
+                    if (handler != null)
+                        handler(ex);
+                    if (count >= 0 && retry >= count)
+                        throw;
+                }
+            }
+        }
+    }
+}
